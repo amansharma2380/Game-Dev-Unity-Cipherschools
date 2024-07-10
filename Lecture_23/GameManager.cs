8acf26906fd30db5b238e174a79fd9e288bd42a5
Aman Sharma <120771366+amansharma2380@users.noreply.github.com>
2024-07-10 11:47:32 +0530

    Create GameManager.cs

---
+++ Lecture_23/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject winPanel;
+    public GameObject losePanel;
+     public GameObject restartButton;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void GameOverScreen(bool hasWon)
+    {
+        if(hasWon) winPanel.SetActive(true);
+        else losePanel.SetActive(true);
+
+        restartButton.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
