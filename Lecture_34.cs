14eeb247a30e666aa2270e05da30635ec7db4c6b
Aman Sharma <120771366+amansharma2380@users.noreply.github.com>
2024-07-10 11:56:12 +0530

    Create Lecture_34.cs

---
+++ Lecture_34.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManager : MonoBehaviour
+{
+    private void Start()
+    {
+    Destroy(GameObject, 2f);
+    }
+
+    private void OnMouseDown()
+    {
+    Debug.Log("HIT");
+    Destroy(GameObject);
+    }
+}
